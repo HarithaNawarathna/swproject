06b6aeda3a25d4b26af50a8e649d64db40a86034
HarithaNawarathna <nawarathnammhr.21@uom.lk>
2024-03-26 22:49:20 +0530

    Add Cancle component

---
+++ src/Pages/Cancle.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const cancle = () => {
+  return (
+    <View>
+      <Text>cancle</Text>
+    </View>
+  )
+}
+
+export default cancle
+
+const styles = StyleSheet.create({})
